97e87ab6257d518a8aa702752fc6e425620c9954
Viorel Zidariu <viorelz@gmail.com>
2021-08-04 12:40:58 +0300

    fix terraform availability zone
    Signed-off-by: Panagiotis Xynos <panos@onlyascii.io>
    Co-authored-by: Panagiotis Xynos <panos@onlyascii.io>

--- ci/terraform/locals.tf
+++ ci/terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   bosh_vpc_cidr = "10.0.0.0/16"
   bosh_public_subnet_cidr = "10.0.1.0/24"
-  bosh_public_subnet_az = "az1"
+  bosh_public_subnet_az = "eu-west-1a"
   bosh_private_subnet_cidr = "10.0.2.0/24"
   bosh_private_subnet_az = "eu-west-1a"
 }
